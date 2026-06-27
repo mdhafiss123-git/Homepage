903558dfd80ad5308788fc9a09da721ed057d46d
Mohamed Hafis <mdhafiss123@gmail.com>
2026-06-27 23:05:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-cream-50">
+    <div className="min-h-screen bg-white">
       <Navbar />
       <main>
         <Hero />
